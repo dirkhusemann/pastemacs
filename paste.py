e4db8df1ab8339e13f6f2dfc81af20d0a65c02c7
Sebastian Wiesner <basti.wiesner@gmx.net>
2007-07-23 12:23:06 +0200

    Changed licence to GPL because of emacs' licence
    committer: Sebastian Wiesner <basti.wiesner@gmx.net>

--- paste.py
+++ paste.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 # allows pasting the current buffer on paste.pocoo.org
-
 # Copyright (c) 2007 Sebastian Wiesner <basti.wiesner@gmx.net>
 
-# This program is free software. It comes without any warranty, to
-# the extent permitted by applicable law. You can redistribute it
-# and/or modify it under the terms of the Do What The Fuck You Want
-# To Public License, Version 2, as published by Sam Hocevar. See
-# http://sam.zoy.org/wtfpl/COPYING for more details.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
 
 
 # TODO: add proper error handling to avoid exception tracebacks
